6b5f7321dfdd442b049744efb7cd08137304a9d8
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-02-04 19:51:31 -0600

    just needs + changed to appropriate letter

--- hw1/bmitchinson/task8/task8.c
+++ hw1/bmitchinson/task8/task8.c
@@ -20,10 +20,25 @@ char* hex(char *word){
   int j = 0;
   for (int i = 0; i < letters; i++){
     int ascii_val = (int) word[i];
-    coded[j] = (char) ascii_val;
-    j++;
-    coded[j] = (char) ascii_val;
-    j++;
+
+    int mod1 = ascii_val % 16;
+    int next = ascii_val / 16;
+    int mod2 = next % 16;
+    if (mod1 > 9) {
+      mod1 = 43;
+    }
+    else {
+      mod1+=48;
+    }
+    if (mod2 > 9) {
+      mod2 = 43;
+    }
+    else {
+      mod2+=48;
+    }
+    coded[j] = (char) mod2;
+    coded[j+1] = (char) mod1;
+    j+=2;
   }
   coded[coded_size-1] = '\0';
 
@@ -31,8 +46,12 @@ char* hex(char *word){
 }
 
 /*
-Example 13 to hex (0D)
-13 / 16 -> 13 -> D second pos
+Example: 13 to hex (0D)
+13 % 16 -> 13 -> D second pos
 ^ aka 13 mod 16? does 0 mod 16 yield 0? If so that's it no edge case.
-0 / 16 -> 0 -> first pos
+0 % 16 -> 0 -> first pos
+
+Example: 101 to hex (65)
+101 / 16 -> 6 r 5
+6 / 16 -> 0 r 6
 */
