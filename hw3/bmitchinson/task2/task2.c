80a4e199a7a118051aab0cf3235d6ad553403de0
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-03-31 16:07:59 -0500

    print letter from mem

--- hw3/bmitchinson/task2/task2.c
+++ hw3/bmitchinson/task2/task2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <sys/user.h>
+#include <err.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
@@ -19,9 +21,27 @@ int main(int arg_count, char** argv) {
   // printf("%s\n",mem);
   // sscanf(assembly_code_raw, "%02hhx", mem);
 
-  byte* mem;
+  // byte* mem;
   char* assembly_code_raw = argv[1];
-  mem = (byte*) mmap(0, 100, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_SHARED, -1, 0);
-  sscanf(assembly_code_raw, "%02hx", mem);
-  ((void (*)(void))mem)();
+  byte *mem = mmap(0, 10000, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+  if (mem == MAP_FAILED){
+    printf("map failed\n");
+    exit(1);
+  }
+  else {
+    printf("we're good\n");
+    // sscanf("h", "%c", mem[0]);
+    // sscanf("i", "%c", mem[1]);
+    // sscanf("\0", "%c", mem[2]);
+    int placed = sscanf("hi", "%s", mem);
+    printf("placed: %i\n", placed);
+    // sscanf(assembly_code_raw[0], "%02hx", mem[0]);
+    printf("scanned\n");
+    printf("mem: %c\n", mem[1]);
+    printf("k\n");
+    // ((void (*)(void))mem)();
+  }
+  // sscanf("e", "%02hx", mem);
+  // for (int i = 0; i < strlen(assembly_code_raw); i++){
+  // }
 }
