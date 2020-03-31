430a8ca6c4b66b826f40e3b77292598470b13ede
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-03-31 11:36:47 -0500

    attempt

--- hw3/bmitchinson/task2/task2.c
+++ hw3/bmitchinson/task2/task2.c
@@ -7,20 +7,21 @@
 #include <stdint.h>
 #include <sys/mman.h>
 
+typedef unsigned char byte;
+
 int main(int arg_count, char** argv) {
-  char* assembly_code_raw = argv[1];
-  void* mem = malloc((strlen(assembly_code_raw) / 2) + 1);
+  // char* assembly_code_raw = argv[1];
+  // void* mem = malloc((strlen(assembly_code_raw) / 2) + 1);
   // printf("%p\n",mem);
   //sscanf("hey", "%s", mem);
-  sscanf(assembly_code_raw, "%02hhx", mem);
-  ((void (*)(void))mem)();
+  // sscanf(assembly_code_raw, "%02hhx", mem);
+  // ((void (*)(void))mem)();
   // printf("%s\n",mem);
   // sscanf(assembly_code_raw, "%02hhx", mem);
-  // void* mem;
-  // char* assembly_code_raw = argv[1];
-  // mem = (void*) mmap(0, 100, 7, MAP_SHARED, -1, 0);
-  // printf("%p\n",mem);
-  // // sscanf(assembly_code_raw, "%02hhx", mem);
-  // sscanf("hey", "%s", mem);
-  // // ((void (*)(void))mem)();
+
+  byte* mem;
+  char* assembly_code_raw = argv[1];
+  mem = (byte*) mmap(0, 100, PROT_EXEC|PROT_READ|PROT_WRITE, MAP_SHARED, -1, 0);
+  sscanf(assembly_code_raw, "%02hx", mem);
+  ((void (*)(void))mem)();
 }
