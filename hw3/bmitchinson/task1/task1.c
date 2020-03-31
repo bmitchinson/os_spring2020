2cba15e690bb22a2d0831ddcf408f081852df8a8
Ben Mitchinson <mitchinson.dev@gmail.com>
2020-03-31 00:05:41 -0500

    full mem range

--- hw3/bmitchinson/task1/task1.c
+++ hw3/bmitchinson/task1/task1.c
@@ -44,22 +44,39 @@ int virtualSelf(char* req_addr_str) {
   char* m_cmd;
   size_t m_cmd_sz;
   #pragma GCC diagnostic ignored "-Wformat="
-  m_cmd_sz = snprintf(NULL, 0, "cat /proc/%lu/maps | grep -e 'heap' -e 'task1'", pid);
+  m_cmd_sz = snprintf(NULL, 0, "cat /proc/%lu/maps", pid);
   m_cmd = malloc(m_cmd_sz + 1);
   #pragma GCC diagnostic ignored "-Wformat="
-  snprintf(m_cmd, m_cmd_sz+1, "cat /proc/%lu/maps | grep -e 'heap' -e 'task1'", pid);
+  snprintf(m_cmd, m_cmd_sz+1, "cat /proc/%lu/maps", pid);
 
   maps_output = popen(m_cmd, "r");
 
   /* Read the output a line at a time - output it. */
   while (fgets(m_scan, sizeof(m_scan), maps_output) != NULL) {
-    char* beg = malloc(8 + 1); 
+    char* beg;
+    char* end;
+    
+    beg = malloc(8 + 1); 
     memcpy(beg, &m_scan[0], 8);
-    char* end = malloc(8 + 1); 
+    end = malloc(8 + 1); 
     memcpy(end, &m_scan[9], 8);
 
-    printf("%s\n", beg);
-    printf("%s\n", end);
+    if(m_scan[0] == '7'){
+      beg = malloc(12 + 1); 
+      end = malloc(12 + 1); 
+      memcpy(beg, &m_scan[0], 12);
+      memcpy(end, &m_scan[13], 12);
+    }
+
+    if(m_scan[0] == 'f'){
+      beg = malloc(16 + 1); 
+      end = malloc(16 + 1); 
+      memcpy(beg, &m_scan[0], 16);
+      memcpy(end, &m_scan[17], 16);
+    }
+
+    // printf("%s\n", beg);
+    // printf("%s\n", end);
     printf("%s", m_scan);
 
     if ((strtol(beg, NULL, 16) <= req_addr) && (req_addr < strtol(end, NULL, 16))){
